b4bef0bf0d33bc990599eaf1e92ccccfdcc9124a
Veronika Benkeser <veronika.benkeser@gmail.com>
2016-01-20 15:30:54 +0000

    Changed socket.io handling

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 var http = require('http');
 var path = require('path');
 var express = require('express');
-var router = express();
-var server = http.createServer(router);
+var app = express();
+var server = http.createServer(app);
 var cp = require("child_process");
 var sio = require("socket.io");
 
-router.use(function(req,res,next){
-  res.setHeader('Access-Control-Allow-Origin','*');
-  res.setHeader('Access-Control-Allow-Methods','GET','POST');
-  res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type,Authorization');
-  next();
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
 });
 
-router.use(express.static(path.resolve(__dirname, 'dist')));
+app.use(express.static(path.resolve(__dirname, 'dist')));
 
-var io = sio.listen(server);
+var io = sio.listen(server, {
+    log: false,
+    origins: '*:*'
+});
 
-var port= process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 server.listen(port, function() {
-   console.log("Server listening on "+port);
+    console.log("Server listening on " + port);
 });
 
 io.on('connection', function(client) {
